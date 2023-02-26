883754fb1c6b58afe2e4a84bcef03a50597edaed
chloemolle <chloe.molle.telecom@gmail.com>
2023-02-26 17:32:04 +0100

    mini fixes

--- lawn-mower/src/types.ts
+++ lawn-mower/src/types.ts
@@ -1,3 +1,5 @@
+import { EAST, WEST, NORTH, SOUTH } from './constants';
+
 export type Position = [number, number];
-export type Direction = 'E' | 'N' | 'W' | 'S';
+export type Direction = [typeof EAST, typeof WEST, typeof NORTH, typeof SOUTH][number];
 export type Correspondance = { [key in Direction]: Direction }

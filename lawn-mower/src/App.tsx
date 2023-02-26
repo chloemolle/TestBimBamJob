8c70f60d7d3135241bcc3e83c3c4677a32d39d74
chloemolle <chloe.molle.telecom@gmail.com>
2023-02-26 15:31:23 +0100

    Read file and print data

--- lawn-mower/src/App.tsx
+++ lawn-mower/src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState<String | ArrayBuffer | null>('');
+
+  const handleFile = (file: File) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(file);
+    fileReader.onload = function() {
+      setData(fileReader.result);
+    };
+  }
+
   return (
     <div className="App">
-      BimBamJob Test
+      BimBamJob Test <div>{`${data}`}</div>
+      <input type="file" onChange={(e) => (e?.target?.files?.length && handleFile(e.target.files[0]))} />
     </div>
   );
 }
